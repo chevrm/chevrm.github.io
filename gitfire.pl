d29544bbd0bff6deb99e33ff4c2a276e2f662122
Marc Chevrette <chevrm@gmail.com>
2015-05-13 21:33:51 -0400

    autofired

--- gitfire.pl
+++ gitfire.pl
@@ -3,14 +3,16 @@
 use strict;
 use warnings;
 
+my $theme = undef;
 my $com = "git commit -m 'autofired'";
 $com =~ s/autofired/$ARGV[0]/ if(@ARGV[0]);
 my @cmd = (
-	"pelican -s pelicanconf.py -o ./ content -t mct",
+	"pelican -s pelicanconf.py -o ./ content",
 	"git add .",
  	$com,
 	"git push -u origin master" 
 );
+$cmd[0] .= " -t $theme" if($theme);
 foreach (@cmd){
 	system("$_");
 }
