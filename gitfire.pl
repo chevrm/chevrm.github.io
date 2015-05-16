5159c2714f69bc743218a213bb8f2aa09290f0b5
Marc Chevrette <chevrm@gmail.com>
2015-05-16 11:56:18 -0400

    autofired20150416

--- gitfire.pl
+++ gitfire.pl
@@ -4,8 +4,13 @@ use strict;
 use warnings;
 
 my $theme = './elegant';
-my $com = "git commit -m 'autofired'";
-$com =~ s/autofired/$ARGV[0]/ if($ARGV[0]);
+my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
+$year += 1900;
+$mon = '0' . $mon if($mon<10);
+$mday = '0' . $mday if($mday<10);
+my $stamp = $year . $mon . $mday;
+my $com = "git commit -m 'autofired$stamp'";
+$com =~ s/autofired$stamp/$ARGV[0]/ if($ARGV[0]);
 my @cmd = (
 	"pelican -s pelicanconf.py -o ./ content",
 	"git add .",
